e8de49dbbebca63c4e341f59ce8b9756f3d10f8e
uniquePluhh <domalavanja@hotmail.com>
2024-06-18 10:52:59 +1000

    Update newscript.bat

--- newscript.bat
+++ newscript.bat
@@ -34,7 +34,7 @@ set savefile=%temp%\clicker_save.txt
 set updateFlag=%temp%\clicker_update.flag
 
 REM Set the URL of the updated script
-set "updateUrl=https://raw.githubusercontent.com/username/repository/branch/newscript.bat"
+set "updateUrl=https://raw.githubusercontent.com/uniquePluhh/Uniuqe-Clicker-Cloud-Auto-Update/main/newscript.bat"
 REM Set the path to the temp file where the new script will be downloaded
 set "tempFile=%temp%\newscript.bat"
 
